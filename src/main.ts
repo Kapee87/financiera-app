e7556aee77d7feee3037a9002dc680926f171b41
Kapee87 <kapserker@gmail.com>
2024-09-24 17:55:38 -0300

    added cors

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const port = parseInt(process.env.BASE_URL.split(':').pop(), 10);
+  const port = parseInt(process.env.PORT || '3000');
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin:
+      process.env.NODE_ENV === 'development'
+        ? 'http://localhost:5173'
+        : process.env.FRONTEND_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   await app.listen(port);
 }
 bootstrap();
